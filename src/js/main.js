21f689d01ff3aaa91d9ef4b3ef2c067de3a0e792
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-30 17:55:02 +0300

    changelog from 300519

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,10 @@
     "use strict"
     $(function() {
 
-        
+        $('.__dd__slide9__carousel__items').slick({
+            prevArrow: '.__dd__slide9__carousel__controls button.is--prev',
+            nextArrow: '.__dd__slide9__carousel__controls button.is--next'
+        })
 
     })
 })(jQuery);
